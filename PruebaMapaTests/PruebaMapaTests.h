776e1dbe98b972ba56bd24e6f02f460ac32ec0fc
Javi <jmoreno.zn@gmail.com>
2012-05-17 01:31:54 +0200

    Initial Commit

---
+++ PruebaMapaTests/PruebaMapaTests.h
@@ -0,0 +1,13 @@
+//
+//  PruebaMapaTests.h
+//  PruebaMapaTests
+//
+//  Created by Javier Moreno on 17/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PruebaMapaTests : SenTestCase
+
+@end
---
+++ PruebaMapaTests/PruebaMapaTests.m
@@ -0,0 +1,32 @@
+//
+//  PruebaMapaTests.m
+//  PruebaMapaTests
+//
+//  Created by Javier Moreno on 17/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PruebaMapaTests.h"
+
+@implementation PruebaMapaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PruebaMapaTests");
+}
+
+@end
