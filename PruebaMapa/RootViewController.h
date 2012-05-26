ed183d1c8ee94d1e09f7ff5a34ee998fad63a4ee
Javi <jmoreno.zn@gmail.com>
2012-05-26 15:45:17 +0200

    Ejemplo de Google Directions
    Pues si que es verdad que no es tan difícil hacer rutas entre dos
    puntos con MapKit

---
+++ PruebaMapa/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  PruebaMapa
+//
+//  Created by Javier Moreno on 25/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ PruebaMapa/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  PruebaMapa
+//
+//  Created by Javier Moreno on 25/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
